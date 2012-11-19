d1e6a17243887628f0dbe1eed21f16ddcef64c63
pandrew <letters@paulnotcom.se>
2012-11-19 15:13:35 +0100

    bootstrap.rb cleanup

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,44 +1,46 @@
 #!/usr/bin/ruby
-#config
 
-ohmyzsh_location = "/Volumes/16GB1/home/paul_andrew/osx/BOOTSTRAP/dotfiles/oh-my-zsh"
-dotfiles_location = "/Volumes/16GB1/home/paul_andrew/osx/BOOTSTRAP/dotfiles"
-rbenv_location = "/Volumes/16GB1/home/paul_andrew/osx/BOOTSTRAP/dotfiles/rbenv"
-bootstrap_location = "/Volumes/16GB1/home/paul_andrew/osx/BOOTSTRAP"
+# Setup variables
+basedir = "$HOME"
+basename = ".source"
+boostrtap ="$basedir/$basename"
+ohmyzsh = "$bootstrap/oh-my-zsh"
+rbenv = "$bootstrap/rbenv"
 
 
-#hostname
+# Create some default paths
+`mkdir $HOME/bin`
+
+# Set hostname
 `sudo scutil --set HostName stewie.lan`
 
 #crontab
-`crontab #{dotfiles_location}/paul_andrew.crontab`
+#`crontab #{dotfiles_location}/paul_andrew.crontab`
 
 
-`mkdir $HOME/bin`
 
 
-unless rbenv_location == ""
+unless rbenv == ""
   puts "Linking rbenv..."
-  `ln -Fs #{rbenv_location} $HOME/.rbenv`
-  `ln -Fs #{dotfiles_location}/zshrc $HOME/.zshrc`
-  
+  `ln -Fs #{rbenv} $HOME/.rbenv`
 end
 
 
 #oh-my-zsh
 unless ohmyzsh_location == ""
-   puts "Linking oh-my-zsh..."
-   `ln -Fs #{ohmyzsh_location} $HOME/.oh-my-zsh`
+	puts "Linking oh-my-zsh..."
+	`ln -Fs #{ohmyzsh} $HOME/.oh-my-zsh`
 end
 unless ENV['SHELL'] == "/bin/zsh"
-     puts "Changing default shell to zsh..."
-     `chsh -s /bin/zsh`
+	puts "Changing default shell to zsh..."
+	`chsh -s /bin/zsh`
+	`ln -Fs #{dotfiles}/zshrc $HOME/.zshrc`
 end
 
 
 #dotfiles
 puts "Linking dotfiles"
-`bash #{bootstrap_location}/linking.sh`
+`bash #{bootstrap}/linking.sh`
 
 #execute brew.sh
 `sh #{bootstrap_location}/brew.sh`
